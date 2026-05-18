0aacf9f0753f1b734dfc8ea426eb6b568a68a445
KodewithArun <contactwitharunlaudari@gmail.com>
2026-05-18 22:04:54 +0545

    day8: listview simple using listtile

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -9,28 +9,28 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: Center(
-        child: InkWell(
-         
-          onTap: () {
-            print("Tapped");
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: Colors.blue,
-            ),
-            height: 50,
-            width: 150,
-            child: Center(
-              child: Text(
-                "Click Me",
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text("Item 1"),
+            subtitle: Text("Description for Item 1"),
+            leading: Icon(Icons.star),
+            trailing: Icon(Icons.arrow_forward_ios),
           ),
-        ),
-      ),
+          ListTile(
+            title: Text("Item 2"),
+            subtitle: Text("Description for Item 2"),
+            leading: Icon(Icons.star_border),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          ListTile(
+            title: Text("Item 3"),
+            subtitle: Text("Description for Item 3"),
+            leading: Icon(Icons.star_half),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+        ],
+      )
     );
   }
 }
