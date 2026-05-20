eab01dba7eb121d26bb63ffc110a879ef3807c4f
KodewithArun <contactwitharunlaudari@gmail.com>
2026-05-20 13:42:12 +0545

    day10: understand about the stack widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -7,62 +7,17 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
-      body: GridView.count(
-        physics: BouncingScrollPhysics(),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
+      body: Stack(
         children: [
           Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 125, 36, 30),
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.blue,
+
           ),
-          Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 32, 182, 54),
-          ),
-          Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 30, 115, 138),
-          ),
-          Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 22, 40, 106),
-          ),
-          Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 121, 67, 81),
-          ),
-          Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 236, 123, 2),
-          ),
-          Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 125, 36, 30),
-          ),
-          Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 32, 182, 54),
-          ),
-          Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 30, 115, 138),
-          ),
-          Container(
-            height: 200,
-            width: 200,
-            color: const Color.fromARGB(255, 22, 40, 106),
-          ),
+          Container(width: 200, height: 200, color: Colors.orange),
+          Container(width: 100, height: 100, color: Colors.white),
+          Container(width: 50, height: 50, color: Colors.red),
         ],
       ),
     );
