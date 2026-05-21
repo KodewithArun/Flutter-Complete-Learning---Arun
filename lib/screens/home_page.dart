90c834882c419a1ba3707b765102990f188ffbd9
KodewithArun <contactwitharunlaudari@gmail.com>
2026-05-21 11:48:49 +0545

    day 11: understand the font awesome icon using pub get

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,10 +8,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Text Widget')),
-      body: const Icon(
-        Icons.headphones,
-        size: 100,
-        color: Color.fromARGB(255, 154, 172, 22),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "This is an Font Awesome Icon",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            FaIcon(
+              FontAwesomeIcons.instagram,
+              size: 50,
+              color: Colors.redAccent,
+            ),
+          ],
+        ),
       ),
     );
   }
