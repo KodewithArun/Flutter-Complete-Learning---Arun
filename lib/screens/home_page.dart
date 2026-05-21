b8faba68ce4e90ed074fb6e4e8acbda84e509e2c
KodewithArun <contactwitharunlaudari@gmail.com>
2026-05-21 11:41:57 +0545

    day11: understand about the Icon Widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,28 +6,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
-      body: RichText(
-        text: TextSpan(
-          text:
-              'Flutter is Google’s mobile app SDK for crafting high-quality native interfaces on iOS and Android in record time.',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-          children: [
-            TextSpan(
-              text:
-                  '\n\nIt allows you to build beautiful apps with a single codebase.',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.normal,
-                color: Colors.grey[700],
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(title: const Text('Text Widget')),
+      body: const Icon(
+        Icons.headphones,
+        size: 100,
+        color: Color.fromARGB(255, 154, 172, 22),
       ),
     );
   }
