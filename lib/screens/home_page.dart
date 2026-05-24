368b8678fb60c41603e23e19941e60991db0aeec
KodewithArun <contactwitharunlaudari@gmail.com>
2026-05-24 12:16:27 +0545

    day12: understand about the gridiant type 1> Linear gridiant

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,37 +1,23 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var arrIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
-      body: ListWheelScrollView(
-        itemExtent: 250,
-        children:
-            arrIndex
-                .map(
-                  (e) => Container(
-                    height: 100,
-                    width: double.infinity,
-                    color: Colors.primaries[e % Colors.primaries.length],
-                    child: Center(
-                      child: Text(
-                        "Item $e",
-                        style: TextStyle(fontSize: 24, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                )
-                .toList(),
+      appBar: AppBar(title: const Text("Home Page")),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 255, 12, 12),
+              Color.fromARGB(255, 255, 255, 255),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
       ),
     );
   }
