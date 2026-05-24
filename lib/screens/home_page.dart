b39b16b3ec1633a32ec78f8bf777af603fe59079
KodewithArun <contactwitharunlaudari@gmail.com>
2026-05-24 11:50:15 +0545

    day12: understand about the animationalign

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,39 +8,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double boxSize = 800;
-  Color boxColor = Colors.blue;
-  double boxOpacity = 1.0;
-
-  void animateBox() {
-    setState(() {
-      boxSize = boxSize == 800 ? 200 : 800;
-      boxColor = boxColor == Colors.blue ? Colors.red : Colors.blue;
-      boxOpacity = boxOpacity == 1.0 ? 0.0 : 1.0;
-    });
-  }
+  Alignment alignment = Alignment.topLeft;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('AnimatedContainer')),
-      body: Center(
-        child: AnimatedOpacity(
-          duration: const Duration(milliseconds: 800),
-          curve: Curves.easeInOut,
-          opacity: boxOpacity,
-          child: AnimatedContainer(
-            duration: const Duration(seconds: 1),
-            curve: Curves.easeInOut,
-            width: boxSize,
-            height: boxSize,
-            color: boxColor,
-          ),
-        ),
+      body: AnimatedAlign(
+        duration: Duration(seconds: 1),
+        curve: Curves.easeInOutCubic,
+        alignment: alignment,
+        child: Container(width: 80, height: 80, color: Colors.green),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: animateBox,
-        child: const Icon(Icons.play_arrow),
+        onPressed: () {
+          setState(() {
+            alignment =
+                alignment == Alignment.topLeft
+                    ? Alignment.bottomRight
+                    : Alignment.topLeft;
+          });
+        },
+        child: Icon(Icons.open_with),
       ),
     );
   }
